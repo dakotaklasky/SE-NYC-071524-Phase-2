d5d43e5a6106d3f05afa3b5c13c56db069fa1f8a
Dakota <dakota.klasky@gmail.com>
2024-08-07 10:00:11 -0400

    my changes

--- pets-app/src/components/PetList.js
+++ pets-app/src/components/PetList.js
@@ -1,10 +1,10 @@
 
 import Pet from "./Pet"
 
-function PetList({id,image,name}){
+function PetList({pets}){
 
     const liElements = pets.map(pet => {
-        return (<Pet key= {id} className='pet' image={image} name ={name}/>)
+        return (<Pet key= {pet.id} className='pet' image={pet.image} name ={pet.name}/>)
     })
 
     return(
